08aa55366d2f592c95a1d15e15ba0b4f54bc23ce
eric-yoo <ajoije729@gmail.com>
2019-05-13 04:41:28 +0900

    import by name

--- contexts/admin.py
+++ contexts/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import *
+from .models import Context,Activity
+admin.site.register(Context)
+admin.site.register(Activity)
