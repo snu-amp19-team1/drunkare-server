8741e1c5f2167f26566f985be491d8602802fa87
eric-yoo <ajoije729@gmail.com>
2019-05-13 03:22:34 +0900

    simple context model

--- contexts/models.py
+++ contexts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Context(models.Model):
+    context_id = models.AutoField(primary_key=True)
+    context_label = models.CharField(max_length=100)
+    
+    class Meta:
+        managed = True
+
+    def __str__(self):
+        return str(self.context_label)
+
+class Activity(models.Model):
+    activity_id = models.AutoField(primary_key=True)
+    activity_label = models.CharField(max_length=100)
+    
+    class Meta:
+        managed = True
+
+    def __str__(self):
+        return str(self.activity_label)
