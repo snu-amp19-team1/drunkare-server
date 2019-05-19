a6c0f6a79a24f2e4fb35369ed5e913d465c61d13
eric-yoo <ajoije729@gmail.com>
2019-05-19 23:50:15 +0900

    rename json, prepare for context prediction

--- contexts/views.py
+++ contexts/views.py
@@ -2,13 +2,20 @@ import json
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from .activity_predictor import predict_activity
+from .activity_predictor import predict_activity, predict_context
+from .models import Context, Activity
 
 @csrf_exempt
 def infer(request):
     if request.method == "POST":
         print("infering")
-        result = predict_activity(model_name="RF")
-        result_dump = json.dumps(result)
-        return JsonResponse({"result":result_dump})
-    
+        
+        activities = predict_activity(model_name="RF")
+        activities_dump = json.dumps(activities)
+
+        activities_label = [Activity.objects.get(activity_id=pred).activity_label for pred in activities]
+        print(activities_label)
+        
+        context = predict_context()
+
+        return JsonResponse({"activities":activities_dump})
