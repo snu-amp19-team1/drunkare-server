4f13837ff38e92b1042a9a7d90516adac5954f19
eric-yoo <ajoije729@gmail.com>
2019-05-19 22:34:36 +0900

    add RawDataRecord model for raw data collection

--- data/models.py
+++ data/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
 # Create your models here.
-# class RawData(model.Model):
-#   don't implement for now, write to file and extract features directly
+class RawDataRecord(models.Model):
+    record_id = models.AutoField(primary_key=True)
+    timestamp = models.DateTimeField()
+    X_acc = models.CharField(max_length=1000)
+    Y_acc = models.CharField(max_length=1000)
+    Z_acc = models.CharField(max_length=1000)
+    X_gyro = models.CharField(max_length=1000)
+    Y_gyro = models.CharField(max_length=1000)
+    Z_gyro = models.CharField(max_length=1000)
+
+    class Meta:
+        managed = True
+    
+    def __str__(self):
+        return str(self.timestamp)
 
 class FeatureRecord(models.Model):
     record_id = models.AutoField(primary_key=True)
