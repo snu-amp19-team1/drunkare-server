d959ae64b167bb6debca12a02c356652ff4bcc71
eric-yoo <ajoije729@gmail.com>
2019-05-19 22:35:25 +0900

    infer on post

--- contexts/views.py
+++ contexts/views.py
@@ -1,11 +1,14 @@
 import json
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .activity_predictor import predict_activity
 
-# Create your views here.
+@csrf_exempt
 def infer(request):
-    print("infering")
-    result = predict_activity(model_name="CONV_LSTM")
-    result_dump = json.dumps(result)
-    return JsonResponse({"result":result_dump})
+    if request.method == "POST":
+        print("infering")
+        result = predict_activity(model_name="RF")
+        result_dump = json.dumps(result)
+        return JsonResponse({"result":result_dump})
+    
