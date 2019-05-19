ccaff4a9252583f593d693f1e16573e6ee1acba9
eric-yoo <ajoije729@gmail.com>
2019-05-19 21:30:26 +0900

    sample inference

--- contexts/views.py
+++ contexts/views.py
@@ -1,8 +1,11 @@
+import json
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from .activity_predictor import predict_activity
 
 # Create your views here.
 def infer(request):
     print("infering")
-    return JsonResponse({'error':'model not ready'})
+    result = predict_activity(model_name="CONV_LSTM")
+    result_dump = json.dumps(result)
+    return JsonResponse({"result":result_dump})
