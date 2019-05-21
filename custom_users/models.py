edbbe55a2583d4a8b4425b8d7da163514b030b34
eric-yoo <ajoije729@gmail.com>
2019-05-21 23:07:28 +0900

    make user_id manual field

--- custom_users/models.py
+++ custom_users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from contexts.models import Activity,Context
 # Create your models here.
 class CustomUser(models.Model):
-    user_id = models.AutoField(primary_key=True)
+    user_id = models.IntegerField(primary_key=True)
     user_name = models.CharField(max_length=100)
     current_activity = models.ForeignKey(Activity,models.DO_NOTHING)
     current_context = models.ForeignKey(Context,models.DO_NOTHING)
