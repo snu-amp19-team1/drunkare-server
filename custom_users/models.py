1c0a9cfcbce8549895f1cdfce9835acb7186f9f3
eric-yoo <ajoije729@gmail.com>
2019-05-21 23:36:19 +0900

    make user current status nullable

--- custom_users/models.py
+++ custom_users/models.py
@@ -4,8 +4,8 @@ from contexts.models import Activity,Context
 class CustomUser(models.Model):
     user_id = models.IntegerField(primary_key=True)
     user_name = models.CharField(max_length=100)
-    current_activity = models.ForeignKey(Activity,models.DO_NOTHING)
-    current_context = models.ForeignKey(Context,models.DO_NOTHING)
+    current_activity = models.ForeignKey(Activity,models.DO_NOTHING, null=True,)
+    current_context = models.ForeignKey(Context,models.DO_NOTHING, null=True,)
 
     class Meta:
         managed = True
