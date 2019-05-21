fc3a5ec02f0868383ade60b0186fbce417789200
eric-yoo <ajoije729@gmail.com>
2019-05-21 22:03:15 +0900

    make activity index manual field

--- contexts/models.py
+++ contexts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Context(models.Model):
-    context_id = models.AutoField(primary_key=True)
+    context_id = models.IntegerField(primary_key=True)
     context_label = models.CharField(max_length=100)
     
     class Meta:
@@ -12,7 +12,7 @@ class Context(models.Model):
         return str(self.context_label)
 
 class Activity(models.Model):
-    activity_id = models.AutoField(primary_key=True)
+    activity_id = models.IntegerField(primary_key=True)
     activity_label = models.CharField(max_length=100)
     
     class Meta:
