901b6d41b66314919778ace846d146e6b06ce256
eric-yoo <ajoije729@gmail.com>
2019-05-23 01:09:52 +0900

    add route for fetching user status update

--- custom_users/urls.py
+++ custom_users/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.config),
+    url(r'^fetch$', views.fetch),
 ]
--- custom_users/views.py
+++ custom_users/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import CustomUser
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
 
 # Create your views here.
-def config(request):
-    return render(request, 'config.html')
+@csrf_exempt
+def fetch(request):
+        
+    users = serializers.serialize('json',CustomUser.objects.all())
+
+    return JsonResponse({"users": users})
