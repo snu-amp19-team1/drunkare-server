8c884ea41358cd8da27522cb4c758adcd7cde1d9
Eric Yoo <ajoije729@gmail.com>
2019-05-23 11:26:53 +0900

    init user context, activity as well

--- db_init/db_user.py
+++ db_init/db_user.py
@@ -1,9 +1,27 @@
 from custom_users.models import CustomUser
+from contexts.models import Context
+from datetime import datetime
 
 users = [
     'shpark',
     'parkj0',
-    ]
+]
+
+recent_activities = [
+    '[0,4,5,1,2,0,6,0,0,4,5,1,2,0,6,0]',
+    '[5,0,1,2,1,2,4,0,6,0,0,6,0,0,4,5]',
+]
+
+contexts=[
+    Context.objects.get(context_id=3),
+    Context.objects.get(context_id=2),
+]
 
 for i,user in enumerate(users):
-    CustomUser.objects.create(user_name=user,user_id = i)
+    CustomUser.objects.create(
+        user_name=user,
+        user_id = i,
+        recent_activities=recent_activities[i],
+        current_context=contexts[i],
+        last_update=datetime.now(),
+        )
