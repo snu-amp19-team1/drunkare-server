24dbb778d61ce0f4495885704ad05f886310db3f
Eric Yoo <ajoije729@gmail.com>
2019-05-23 13:58:21 +0900

    pseudo serialization for context

--- custom_users/views.py
+++ custom_users/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from .models import CustomUser
+from contexts.models import Context
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
+import json
 
 # Create your views here.
 @csrf_exempt
 def fetch(request):
-        
     users = serializers.serialize('json',CustomUser.objects.all())
-
-    return JsonResponse({"users": users})
+    users = json.loads(users)
+    for user in users:
+        label=Context.objects.get(context_id=user['fields']['current_context'])
+        user['fields']['current_context']=str(label)
+    return JsonResponse({"users": json.dumps(users)})
