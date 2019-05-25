0c07278eabbf256f2474ea7183addf413bb4597e
Eric Yoo <ajoije729@gmail.com>
2019-05-25 22:53:16 +0900

    allow blank

--- custom_users/models.py
+++ custom_users/models.py
@@ -6,8 +6,8 @@ class CustomUser(models.Model):
     user_name = models.CharField(max_length=100)
     recent_activities = models.CharField(max_length=100, blank=True,)
     current_location = models.CharField(max_length=100, blank=True)
-    current_context = models.ForeignKey(Context,models.DO_NOTHING, null=True,)
-    last_update = models.DateTimeField(null=True,)
+    current_context = models.ForeignKey(Context,models.DO_NOTHING, null=True,blank=True,)
+    last_update = models.DateTimeField(null=True,blank=True,)
 
     class Meta:
         managed = True
