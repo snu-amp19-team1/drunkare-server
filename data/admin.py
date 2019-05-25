bd34e0ed4a35d06909816aee6feb9cefec37139a
Eric Yoo <ajoije729@gmail.com>
2019-05-25 19:30:52 +0900

    register ActivityInferenceRecord table

--- data/admin.py
+++ data/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import FeatureRecord, RawDataRecord
+from .models import FeatureRecord, RawDataRecord, ActivityInferenceRecord
 admin.site.register(FeatureRecord)
 admin.site.register(RawDataRecord)
+admin.site.register(ActivityInferenceRecord)
