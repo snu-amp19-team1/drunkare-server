944ce00baecc6c002b33ffb5c0c828d4f367f912
Eric Yoo <ajoije729@gmail.com>
2019-05-29 16:49:22 +0900

    remove update route

--- contexts/views.py
+++ contexts/views.py
@@ -3,12 +3,6 @@ from .predictor import predict_minute
 from .models import Activity
 from custom_users.models import CustomUser
 
-def update(data):
-        activities = predict_minute(data,model_name="RF")
-        activities_label = [Activity.objects.get(activity_id=pred).activity_label for pred in activities]
-
-        return activities, activities_label
-
 def demo(request):
         activity_labels = [activity.activity_label for activity in Activity.objects.all()]
         user1 = CustomUser.objects.all()[0]
