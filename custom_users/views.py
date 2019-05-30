7929950c8a6203581f476241987383a971cb5f1d
Eric Yoo <ajoije729@gmail.com>
2019-05-30 21:02:42 +0900

    app route

--- custom_users/views.py
+++ custom_users/views.py
@@ -1,13 +1,16 @@
 from .models import CustomUser
-from contexts.models import Context
+from contexts.models import Context, Activity
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
-import json
+from custom_users.models import CustomUser
+from django.utils.timezone import make_aware
+import json, ast
+from datetime import timedelta
 
 # Create your views here.
 @csrf_exempt
-def fetch(request):
+def web(request):
     users = serializers.serialize('json',CustomUser.objects.filter(user_id__lte=1))
     users = json.loads(users)
     for user in users:
@@ -17,3 +20,21 @@ def fetch(request):
         except:
             user['fields']['current_context']=''
     return JsonResponse({"users": json.dumps(users)})
+
+
+# Create your views here.
+@csrf_exempt
+def app(request):
+    user_id = request.GET['user_id']
+    custom_user = CustomUser.objects.get(user_id=user_id)
+    try:
+        context = custom_user.current_context.context_label
+    except:
+        context = ''
+    activity_labels = [activity.activity_label for activity in Activity.objects.all()]
+    activity_probs = ast.literal_eval(custom_user.recent_activities)
+    
+    activity_dict = dict(zip(activity_labels, activity_probs))
+    top3 = sorted(activity_dict.items() ,  key=lambda x: -x[1])[:3]
+    time = (custom_user.last_update+timedelta(hours=9)).strftime("%Y-%m-%d %H:%M:%S")
+    return JsonResponse({'context':context, 'top3':top3, 'time':time})
