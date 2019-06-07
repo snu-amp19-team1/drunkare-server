4b52d5988c50c60ed0904e56a677524a6c559f3d
Eric Yoo <ajoije729@gmail.com>
2019-06-07 11:10:10 +0900

    credentials

--- contexts/views.py
+++ contexts/views.py
@@ -7,5 +7,7 @@ def demo(request):
         activity_labels = [activity.activity_label for activity in Activity.objects.all()]
         user1 = CustomUser.objects.all()[0]
         user2 = CustomUser.objects.all()[1]
-
-        return render(request, 'demo.html', {'activity_labels':activity_labels, 'user1':user1, 'user2':user2, 'gps_key': 'AIzaSyDwLZCrnSp5iiWtoRVYxI0J1we0QtAyT0c '})
+        gps_key=""
+        with open ('gps_key.txt') as f:
+                gps_key = f.read()
+        return render(request, 'demo.html', {'activity_labels':activity_labels, 'user1':user1, 'user2':user2, 'gps_key': gps_key})
