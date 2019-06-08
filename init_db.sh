1dcb2a4d90d84ccdf0b9f87ba9bd6d2ae4d293ac
Eric Yoo <ajoije729@gmail.com>
2019-06-08 21:36:41 +0900

    deafult user

--- init_db.sh
+++ init_db.sh
@@ -2,3 +2,5 @@ cat db_init/clear_data.py | python manage.py shell
 cat db_init/init_activity.py | python manage.py shell
 cat db_init/init_context.py | python manage.py shell
 cat db_init/init_user.py | python manage.py shell
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', '', 'drunkare!@#$ ')" | python manage.py shell
